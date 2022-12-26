eecfd3d24de5955c2032ab6c83d36e48f32e84fe
xShadow <103582788+zShadowSkilled1@users.noreply.github.com>
2022-12-26 15:56:50 +0100

    Update toggle.js

--- toggle.js
+++ toggle.js
@@ -2,5 +2,5 @@ const toggle = document.querySelector('.toggle input')
 
 toggle.addEventListener('click', () => {
     const onOff = toggle.parentNode.querySelector('.onoff')
-    onOff.textContent = toggle.checked ? 'ON' : 'OFF'
+    onOff.textContent = toggle.checked ? 'ON' print("lol") : 'OFF'
 })
